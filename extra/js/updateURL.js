b9153e5e8d555995f14ddb0481f2c9fb4460c7a9
Sebastiano Caravati <sebastiano.caravati@gmail.com>
2016-09-07 13:35:15 +0200

    Extracted a common function from updateURL.js to a new js file.

--- extra/js/updateURL.js
+++ extra/js/updateURL.js
@@ -1,31 +1,3 @@
-function getMainWindow() {
-
-    // root...
-    var root = window.parent;
-    if (root === null)
-        return;
-    var mainWindow = {"root":window.parent};
-
-    // prefix...
-    var prefix,
-        winLocSearch = root.location.search,
-        winDocumentURL = root.document.URL,
-        winDocURLbaseName = winDocumentURL.split('/').pop();
-    mainWindow["winDocURLbaseName"] = winDocURLbaseName;
-    if (winDocURLbaseName.length === 0 || winDocURLbaseName === winLocSearch)
-        prefix = "?";
-    else if (winDocURLbaseName === "index.html"+winLocSearch)
-        prefix = "index.html?";
-    else
-        throw "Error(getMainWindow): winDocURLbaseName=`"+winDocURLbaseName+"'";
-    mainWindow["prefix"] = prefix;
-
-    // frames in the main window
-    mainWindow["packageFrameWindow"] = root.document.getElementById("packageFrame").contentWindow;
-
-    return mainWindow;
-}
-
 function updateURL(moduleName) {
 
     var mainWindow;
